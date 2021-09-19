69651653896aafd1e13952f81bb45282935948c9
Logan Ruzzier <lruzzier@DESKTOP-MQGMU3Q.localdomain>
2021-09-19 17:07:58 -0400

    finished homework

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+  printf("Good News Everyone!\n");
+  int a=1;
+  long b=200;
+  float c=3.0;
+  double d=4.0;
+  char e='5';
+  printf("these are numbers: %d %ld %f %lf %c \n", a,b,c,d,e);
+  unsigned f=6000;
+  char g=777;
+  printf("%d %c\n",f,g);
+  return 0;
+}
